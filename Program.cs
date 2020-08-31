2a1550f8b1d9caa163ebc864ad2f12004fe8dd26
Andrew-JIEYAO <s12308920002@gmail.com>
2020-08-31 16:45:38 +0800

    v3

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace test0831
             //Console.WriteLine("Hello World!");
 
             //demo test
+            //demo test2
             var db = new MyDatabase01Context();
             var ret = from c in db.TAddressBook
                       select c;
